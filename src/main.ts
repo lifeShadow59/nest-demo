3f141a518fa43f4068223b83d1ccaeaee2fd9503
Abhishek Dhanani <40498553+lifeShadow59@users.noreply.github.com>
2022-01-02 01:20:30 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000, "155.248.251.5);
+  await app.listen(3000, "155.248.251.5");
 }
 bootstrap();
